945984ca9ffea101c2933e5675c01f5d3e9ce8af
MuhammadMouradG <muhammad.mourad1995@gmail.com>
2021-06-17 05:53:36 +0200

    Implement getRegistration function in controllers

--- controllers/student.js
+++ controllers/student.js
@@ -24,8 +24,53 @@ module.exports = {
     },
 
     getRegistration: async (req, res) => {
-        // ..
-    },
+		let result;
+		const studentID = req.params.id;
+		const student = await sqlQuery(`SELECT * FROM student
+			WHERE id=('${studentID}')`
+		);
+
+		const allowedCourses = await sqlQuery(`SELECT pre.course_id AS id
+			FROM prerequisite AS pre
+			INNER JOIN (SELECT course_id
+				  FROM registration
+				  WHERE student_id=('${studentID}')
+					AND mark >= 60
+				) AS passedCourses
+			ON pre.pre_id=passedCourses.course_id`
+		);
+		console.log(allowedCourses.length);
+
+		if (allowedCourses.length > 0) {
+			result = await sqlQuery(`SELECT DISTINCT course.*
+				FROM course, student
+				WHERE ((('${allowedCourses[0].id}')=course.id
+						  AND student.semester=course.semester
+					  )
+					OR (course.id NOT IN (SELECT course_id
+										FROM prerequisite
+									   )
+								AND student.semester=course.semester
+					)
+				)`
+			);
+		} else {
+			result = await sqlQuery(`SELECT DISTINCT course.*
+				FROM course, student
+				WHERE course.id NOT IN (SELECT course_id
+									FROM prerequisite
+								   )
+					AND student.semester>=course.semester`
+			);
+		};
+
+
+		return res.render('student/register', {
+			activePath: '/students',
+			student: student[0],
+			allowedCourses: result
+		});
+	},
 
     postRegistration: async (req, res) => {
         // ..
